99d8d3f5d1d49bd6da8d9e5693a6fb8d546f0a87
Hashim <hashimabdrehman@gmail.com>
2024-12-13 15:06:49 +0300

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import '@mantine/core/styles.css';
+import Navbar from './Navbar';
+
+function App() {
+
+
+  return (
+    <>
+    <div className='max-w-screen-2xl px-5 mx-auto h-fit '>
+      <Navbar/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar(){
+    return(
+        <>
+        <div class="flex justify-between">
+      <a href="/">
+        <div class="text-center">
+          <span class="text-black text-xl md:text-2xl font-extrabold ">Vahicle </span><span class="text-sky-500 text-xl md:text-2xl font-extrabold">Management </span>
+          
+          <span class="block md:inline-block text-sky-500 text-xl md:text-2xl font-extrabold">Dashboard</span>
+        </div>
+      </a>
+  
+
+  
+  
+</div>
+        </>
+    )
+}
+export default Navbar;
